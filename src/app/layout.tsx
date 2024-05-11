4538dce81e3b08d3bbac7ccc4fd4c25352b5ecb4
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-05-11 18:46:45 +0700

    feat: setup nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Noteworthy",
+  description: "A pLatform for sharing create notes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
