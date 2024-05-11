7744c61c3a140232831e662b6981385eb9c27243
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-05-11 23:18:34 +0700

    feat: add sign-in functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { auth, signIn } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <main>Home Page</main>;
+export default async function Home() {
+  const session = await auth();
+  return (
+    <main>
+      <form
+        action={async () => {
+          "use server";
+
+          if (!session) {
+            await signIn();
+          } else {
+            redirect("/dashboard");
+          }
+        }}
+      >
+        <Button type="submit">Get Started</Button>
+      </form>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </main>
+  );
 }
